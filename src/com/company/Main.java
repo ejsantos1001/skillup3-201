5ca2ab79103163d04b34cea66ac5a33c483bae78
Ej-PC\Ej <eliazersantos.mail@gmail.com>
2017-03-09 03:39:44 +0800

    intellij files

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,7 @@ public class Main {
 	// write your code here
         System.out.println(fahrenheitToCelcius(1));
         System.out.println(minutesToYearsAndDays(525600 + 1440));
+        System.out.println(isVoter(18));
     }
     public static double fahrenheitToCelcius(double farenheit) {
         return  ((farenheit - 31 ) * 5) / 9 ;
@@ -14,5 +15,14 @@ public class Main {
         int years = minutes / 525600;   // there are 525600 minutes per 1 year
         int days = (minutes % 525600) / 1440;  // there are 1440 minutes in 1 day
         return "There are " + years + " year/s and " + days + " day/s";
+    }
+    public static String isVoter(int age) {
+        String ans = "";
+        if(age>=18 && age <= 110) {
+            ans = "YES";
+        } else if (age < 18 && age > 0) {
+            ans = "NO";
+        }
+        return ans;
     }
 }
