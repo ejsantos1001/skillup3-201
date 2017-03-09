ffd761716f4505682e409e28a9a16a2d3700b60a
Ej-PC\Ej <eliazersantos.mail@gmail.com>
2017-03-09 14:49:13 +0800

    fixed, issue#5

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,20 +1,47 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println(fahrenheitToCelcius(1));
-        System.out.println(minutesToYearsAndDays(525600 + 1440));
-        System.out.println(isVoter(18));
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("input letter of your choice a)fahrenheitToCelcius  b)minutesToYearsAndDays c)isVoter :");
+        String ans = scanner.next();
+        chooseProgram(ans);
+
+
     }
+    public static void chooseProgram(String choice) {
+        Scanner scanner2 = new Scanner(System.in);
+        switch(choice) {
+            case "a" :
+                System.out.println("please enter a fahrentheit value:");
+                double input =  scanner2.nextDouble();
+                System.out.println(fahrenheitToCelcius(input));
+                break;
+            case "b" :
+                System.out.println("enter a certain amount of minutes:");
+                int input2 = scanner2.nextInt();
+                System.out.println(minutesToYearsAndDays(input2));
+                break;
+            case "c" :
+                System.out.println("enter your age");
+                int input3 = scanner2.nextInt();
+                System.out.println(isVoter(input3));
+        }
+
+    }
+
     public static double fahrenheitToCelcius(double farenheit) {
         return  ((farenheit - 31 ) * 5) / 9 ;
     }
     public static String minutesToYearsAndDays(int minutes) {
         int years = minutes / 525600;   // there are 525600 minutes per 1 year
         int days = (minutes % 525600) / 1440;  // there are 1440 minutes in 1 day
-        return "There are " + years + " year/s and " + days + " day/s";
+        return "There are " + years + " year/s and " + days + " day/s in " + minutes + " minute/s";
     }
     public static String isVoter(int age) {
         String ans = "";
